264642ee3a757279b6def4a90d9d0a56a3e1a7a8
Esteban <Esteban@DESKTOP-5IFP0HQ>
2019-11-07 16:03:53 -0300

    menu

--- menuNuevo.js
+++ menuNuevo.js
@@ -6,14 +6,14 @@ function openCity(cityName, elmnt, color) {
     tabcontent[i].style.display = "none";
   }
 
-  // Remove the background color of all tablinks/buttons
+  // Remove the background color of all tablinks/buttonss
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
 
   // Show the specific tab content
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(cityName).style.display = "block"; 
 
   // Add the specific color to the button used to open the tab content
   elmnt.style.backgroundColor = color;
